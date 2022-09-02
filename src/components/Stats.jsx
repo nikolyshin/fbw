ee37a9d18c2da10e3324853c7376be010dacdac4
nikolyshin <nikolyshin@gmail.com>
2022-09-02 13:55:50 +0300

    add stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,11 +1,35 @@
-import { fetchWarehousesBackground, fetchWarehousesOrders } from '../api';
+import {
+  fetchWarehouses,
+  fetchWarehousesBackground,
+  fetchWarehousesOrders
+} from '../api';
 import React, { useEffect, useMemo, useState } from 'react';
 import { Spin, Table, Alert } from 'antd';
+
+const staticColumns = [
+  {
+    title: 'Категории',
+    dataIndex: 'category',
+    key: 'category'
+  },
+  {
+    title: 'Имя',
+    dataIndex: 'subject',
+    key: 'subject'
+  },
+  {
+    title: 'Артикул WB',
+    dataIndex: 'article_wb',
+    key: 'article_wb'
+  }
+];
 
 const Stats = ({ currentWbKey }) => {
   const [loadingOrders, setLoadingOrders] = useState(false);
   const [loadingBackground, setLoadingBackground] = useState(false);
   const [error, setError] = useState('');
+  const [columnsBackground, setColumnsBackground] = useState([]);
+  const [dataSource, setDataSource] = useState([]);
   const [warehousesBackground, setWarehousesBackground] = useState([]);
   const [warehousesOrders, setWarehousesOrders] = useState([]);
 
@@ -16,7 +40,7 @@ const Stats = ({ currentWbKey }) => {
         wbKey: currentWbKey
       });
       if (!res.detail) {
-        setWarehousesOrders(res);
+        setWarehousesOrders(res.results);
       } else {
         setError(res.detail);
       }
@@ -30,7 +54,7 @@ const Stats = ({ currentWbKey }) => {
   const getWarehousesBackground = async () => {
     try {
       setLoadingBackground(true);
-      const res = await fetchWarehousesBackground({
+      const res = await fetchWarehouses({
         wbKey: currentWbKey
       });
       if (!res.detail) {
@@ -47,44 +71,75 @@ const Stats = ({ currentWbKey }) => {
 
   useEffect(() => {
     getWarehousesOrders();
-    getWarehousesBackground();
   }, [currentWbKey]);
 
-  const columnsBackground = useMemo(() => {
-    if (!!warehousesBackground.length) {
-      const keys = Object.keys(warehousesBackground[0]);
+  useEffect(() => {
+    getWarehousesBackground();
+  }, []);
 
-      return keys.map((item) => {
-        return {
-          title: item,
-          dataIndex: item,
-          key: item
-        };
+  useEffect(() => {
+    if (!!warehousesBackground.length) {
+      const arr = [];
+      warehousesBackground.forEach((item) => {
+        arr.push(
+          {
+            title: `Количество на складе ${item.name}`,
+            dataIndex: `count_${item.id}`,
+            key: `count_${item.id}`
+          },
+          {
+            title: `Продаж за период складе ${item.name}`,
+            dataIndex: `sales_${item.id}`,
+            key: `sales_${item.id}`
+          }
+        );
       });
+
+      setColumnsBackground(arr);
     }
   }, [warehousesBackground]);
 
-  const columnsOrders = useMemo(() => {
+  useEffect(() => {
     if (!!warehousesOrders.length) {
-      const keys = Object.keys(warehousesOrders[0]);
-
-      return keys.map((item) => {
-        return {
-          title: item,
-          dataIndex: item,
-          key: item
-        };
+      const arr = [];
+      warehousesOrders.forEach((item) => {
+        const obj = { ...item };
+        item.stocks.map((el) => {
+          obj[`sales_${el.warehouse_id}`] = el.orders;
+          obj[`count_${el.warehouse_id}`] = el.quantity;
+        });
+        arr.push(obj);
       });
+      console.log(arr);
+      setDataSource(arr);
     }
   }, [warehousesOrders]);
+
+  // const columnsOrders = useMemo(() => {
+  //   if (!!warehousesOrders.length) {
+  //     const keys = Object.keys(warehousesOrders[0]);
+
+  //     return keys.map((item) => {
+  //       return {
+  //         title: item,
+  //         dataIndex: item,
+  //         key: item
+  //       };
+  //     });
+  //   }
+  // }, [warehousesOrders]);
 
   return (
     <>
       <Spin spinning={loadingBackground}>
-        <Table columns={columnsBackground} dataSource={warehousesBackground} />
+        {/* <Table columns={columnsBackground} dataSource={warehousesBackground} /> */}
       </Spin>
       <Spin spinning={loadingOrders}>
-        <Table columns={columnsOrders} dataSource={warehousesOrders} />
+        <Table
+          scroll={{ x: true }}
+          columns={[...staticColumns, ...columnsBackground]}
+          dataSource={dataSource}
+        />
       </Spin>
       {!!error && <Alert closable message={error} type="error" />}
     </>
