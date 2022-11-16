af1510db956946abe6be1fefc1f0f21c4fb5b106
nikolyshin <nikolyshin@gmail.com>
2022-11-16 23:28:23 +0300

    change locale

--- src/index.js
+++ src/index.js
@@ -4,36 +4,18 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
+import { ConfigProvider } from 'antd';
+import ruRU from 'antd/es/locale/ru_RU';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CookiesProvider>
       <BrowserRouter>
-        <App />
+        <ConfigProvider locale={ruRU}>
+          <App />
+        </ConfigProvider>
       </BrowserRouter>
     </CookiesProvider>
   </React.StrictMode>
 );
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-// import { CookiesProvider } from 'react-cookie';
-// import { ConfigProvider } from 'antd';
-// import ruRU from 'antd/es/locale/ru_RU';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <CookiesProvider>
-//       <BrowserRouter>
-//         <ConfigProvider locale={ruRU}>
-//           <App />
-//         </ConfigProvider>
-//       </BrowserRouter>
-//     </CookiesProvider>
-//   </React.StrictMode>
-// );
