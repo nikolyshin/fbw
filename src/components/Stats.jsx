1bfb9221016c4ac06518264fe72c90d8b2633d5d
nikolyshin <nikolyshin@gmail.com>
2022-11-24 18:40:29 +0300

    fix filters

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -44,7 +44,7 @@ const Stats = ({
     JSON.parse(localStorage.getItem(nameOfStoreColumnsWhs)) || []
   );
   const [goods, setGoods] = useState([]);
-  const [filters, setFilters] = useState({});
+  const [filters, setFilters] = useState(null);
   const inputRef = useRef([]);
 
   const handlerCreateIncomes = async ({ value, record, item }) => {
@@ -198,89 +198,91 @@ const Stats = ({
   }, [goods]);
 
   useEffect(() => {
-    setColumnsOrders([
-      {
-        title: names.wb_key,
-        dataIndex: 'wb_key',
-        fixed: 'left',
-        width: 150,
-        filterSearch: true,
-        filters: filters?.wb_key_names?.map((item) => {
-          return { text: item, value: item };
-        })
-      },
-      {
-        title: names.category,
-        dataIndex: 'category',
-        fixed: 'left',
-        width: 150,
-        filterSearch: true,
-        filters: filters?.categories?.map((item) => {
-          return { text: item, value: item };
-        })
-      },
-      {
-        title: names.subject,
-        dataIndex: 'subject',
-        fixed: 'left',
-        width: 150,
-        filterSearch: true,
-        filters: filters?.subjects?.map((item) => {
-          return { text: item, value: item };
-        })
-      },
-      {
-        title: names.brand,
-        dataIndex: 'brand',
-        filterSearch: true,
-        filters: filters?.brands?.map((item) => {
-          return { text: item, value: item };
-        }),
-        width: 150
-      },
-      {
-        title: names.article_1c,
-        dataIndex: 'article_1c',
-        filterSearch: true,
-        filters: filters?.articles_1c?.map((item) => {
-          return { text: item, value: item };
-        }),
-        width: 200
-      },
-      {
-        title: names.barcode,
-        filterSearch: true,
-        filters: filters?.barcodes?.map((item) => {
-          return { text: item, value: item };
-        }),
-        dataIndex: 'barcode',
-        width: 150
-      },
+    if (filters) {
+      setColumnsOrders([
+        {
+          title: names.wb_key,
+          dataIndex: 'wb_key',
+          fixed: 'left',
+          width: 150,
+          filterSearch: true,
+          filters: filters?.wb_key_names?.map((item) => {
+            return { text: item, value: item };
+          })
+        },
+        {
+          title: names.category,
+          dataIndex: 'category',
+          fixed: 'left',
+          width: 150,
+          filterSearch: true,
+          filters: filters?.categories?.map((item) => {
+            return { text: item, value: item };
+          })
+        },
+        {
+          title: names.subject,
+          dataIndex: 'subject',
+          fixed: 'left',
+          width: 150,
+          filterSearch: true,
+          filters: filters?.subjects?.map((item) => {
+            return { text: item, value: item };
+          })
+        },
+        {
+          title: names.brand,
+          dataIndex: 'brand',
+          filterSearch: true,
+          filters: filters?.brands?.map((item) => {
+            return { text: item, value: item };
+          }),
+          width: 150
+        },
+        {
+          title: names.article_1c,
+          dataIndex: 'article_1c',
+          filterSearch: true,
+          filters: filters?.articles_1c?.map((item) => {
+            return { text: item, value: item };
+          }),
+          width: 200
+        },
+        {
+          title: names.barcode,
+          filterSearch: true,
+          filters: filters?.barcodes?.map((item) => {
+            return { text: item, value: item };
+          }),
+          dataIndex: 'barcode',
+          width: 150
+        },
 
-      {
-        title: names.article_wb,
-        dataIndex: 'article_wb',
-        filterSearch: true,
-        filters: filters?.articles_wb?.map((item) => {
-          return { text: item, value: item };
-        }),
-        width: 150,
-        render: (text) => (
-          <a
-            href={`https://www.wildberries.ru/catalog/${text}/detail.aspx`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {text}
-          </a>
-        )
-      },
-      {
-        title: names.stock,
-        dataIndex: 'stock',
-        width: 150
-      }
-    ]);
+        {
+          title: names.article_wb,
+          dataIndex: 'article_wb',
+          filterSearch: true,
+          filters: filters?.articles_wb?.map((item) => {
+            return { text: item, value: item };
+          }),
+          width: 150,
+          render: (text) => (
+            <a
+              href={`https://www.wildberries.ru/catalog/${text}/detail.aspx`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {text}
+            </a>
+          )
+        },
+        {
+          title: names.stock,
+          dataIndex: 'stock',
+          width: 150
+        }
+      ]);
+    }
   }, [filters, goods]);
 
   useEffect(() => {
